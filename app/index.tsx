73ed8619847fb12d9970c4e26a103c3e3e01d9d2
notPrasantesp17 <punkersyt@gmail.com>
2024-08-21 22:32:42 +0545

    validation for the login page is done

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ import { Logo } from '../components/utils/constants';
 export default function App() {
   return (
     <GestureHandlerRootView>
-    <View className='flex-col items-center bg-gray-100 justify-evenly  h-[700px]'>
+    <View className='flex-col items-center bg-gray-100 justify-evenly  h-[800px]'>
       <View className='flex-col  h-[50px]'>
         <Image className='w-72 h-20' source={Logo} />
         <Text className='text-lg  font-semibold text-gray-500 text-center '>Save Smart,Live Wise!</Text>
--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -2,17 +2,18 @@ import {  useRouter } from 'expo-router';
 import { Formik } from 'formik';
 import {  Text, TouchableOpacity, View } from 'react-native';
 import {  TextInput } from 'react-native-gesture-handler';
+import { LoginSchema } from './helper/loginSchema';
 
 export default function Loginpage() {
   const router=useRouter();
-  console.log("Router Object:", router);
-  const handlesubmit=()=>{
+   const handlesubmit=()=>{
     router.navigate('/(drawer)/(tabs)/')
   }
   return (
     <Formik
     initialValues={{username:'',password:''}}
     onSubmit={handlesubmit}
+    validationSchema={LoginSchema}
     >
     
     {({
@@ -24,10 +25,11 @@ export default function Loginpage() {
         handleSubmit,
         handleReset
       }) => (<View className='flex-col items-center justify-center w-[85%] h-[55%]   shadow-xl rounded bg-white'>
-      <Text className='text-xl  font-bold text-purple-600 p-1 m-2'>Login to your Account</Text>
+      <Text className='text-xl  font-bold text-purple-600 p-1 m-2 my-4'>Login to your Account</Text>
       <TextInput onChangeText={handleChange('username')} value={values.username} placeholder='Enter Username' inputMode='text' className='p-2 border-2 w-56 rounded-md  border-gray-300 m-3'></TextInput>
+      {errors.username&&<Text className='text-red-600 text-md '>{errors.username}</Text>}
       <TextInput onChangeText={handleChange('password')} value={values.password} placeholder='Enter password' inputMode='text' secureTextEntry className='p-2 border-2 w-56 rounded-md  border-gray-300 m-3'></TextInput>
-      
+      {errors.password&&<Text className='text-red-600 text-md '>{errors.password}</Text>}
         <TouchableOpacity onPress={()=>handleSubmit()}><Text className='text-white bg-purple-600 text-2xl rounded border-white w-28 text-center font-bold m-2 p-1 '>
           login
           </Text></TouchableOpacity>
