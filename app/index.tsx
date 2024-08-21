b3d2e0f0a5476fb65919a86e7cef5f2e676a66f9
notPrasantesp17 <punkersyt@gmail.com>
2024-08-21 20:28:34 +0545

    functionality for the login page is done valid... remaining

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,18 @@
-import { router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import {  Text, View } from 'react-native';
-import { GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Loginpage from '../components/LoginPage';
+import { Image } from 'react-native';
+import { Logo } from '../components/utils/constants';
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-    <View className='flex-col items-center bg-gray-100 justify-center h-[800px]'>
+    <View className='flex-col items-center bg-gray-100 justify-evenly  h-[700px]'>
+      <View className='flex-col  h-[50px]'>
+        <Image className='w-72 h-20' source={Logo} />
+        <Text className='text-lg  font-semibold text-gray-500 text-center '>Save Smart,Live Wise!</Text>
+        </View>
      <Loginpage/>
       <StatusBar  />
     </View>
--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -1,19 +1,44 @@
 import {  useRouter } from 'expo-router';
-import {  Text, View } from 'react-native';
+import { Formik } from 'formik';
+import {  Text, TouchableOpacity, View } from 'react-native';
 import {  TextInput } from 'react-native-gesture-handler';
 
 export default function Loginpage() {
   const router=useRouter();
   console.log("Router Object:", router);
+  const handlesubmit=()=>{
+    router.navigate('/(drawer)/(tabs)/')
+  }
   return (
-    <View className='flex-col items-center justify-center w-[85%] h-[60%]   shadow-xl rounded bg-white'>
-      <TextInput placeholder='Enter Username' inputMode='text' className='p-2 border-2 w-56 rounded-md  border-gray-300 m-3'></TextInput>
-      <TextInput placeholder='Enter password' inputMode='text' secureTextEntry className='p-2 border-2 w-56 rounded-md  border-gray-300 m-3'></TextInput>
+    <Formik
+    initialValues={{username:'',password:''}}
+    onSubmit={handlesubmit}
+    >
+    
+    {({
+        values,
+        errors,
+        touched,
+        isValid,
+        handleChange,
+        handleSubmit,
+        handleReset
+      }) => (<View className='flex-col items-center justify-center w-[85%] h-[55%]   shadow-xl rounded bg-white'>
+      <Text className='text-xl  font-bold text-purple-600 p-1 m-2'>Login to your Account</Text>
+      <TextInput onChangeText={handleChange('username')} value={values.username} placeholder='Enter Username' inputMode='text' className='p-2 border-2 w-56 rounded-md  border-gray-300 m-3'></TextInput>
+      <TextInput onChangeText={handleChange('password')} value={values.password} placeholder='Enter password' inputMode='text' secureTextEntry className='p-2 border-2 w-56 rounded-md  border-gray-300 m-3'></TextInput>
       
-        <Text onPress={()=>router.push('./(drawer)/(tabs)/')} className='text-white bg-purple-600 text-2xl rounded border-white w-28 text-center font-bold m-2 p-1 '>
+        <TouchableOpacity onPress={()=>handleSubmit()}><Text className='text-white bg-purple-600 text-2xl rounded border-white w-28 text-center font-bold m-2 p-1 '>
           login
-          </Text>
-    </View>
+          </Text></TouchableOpacity>
+          <TouchableOpacity><Text className='text-lg  font-semibold text-gray-500 p-1 '>Forget password?</Text></TouchableOpacity>
+          <View className='flex-row items-center justify-center my-2 space-x-1'>
+          <Text className='text-lg  font-semibold text-black  '>New here?</Text>
+          <TouchableOpacity><Text className='text-lg  font-semibold text-gray-500  '>Sign Up</Text></TouchableOpacity>
+          </View>
+    </View>)
+}
+    </Formik>
   );
 }
 
