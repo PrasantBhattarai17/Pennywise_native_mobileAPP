de8463f349fe8ecefd9c99410fad4d850b854bb7
notPrasantesp17 <punkersyt@gmail.com>
2024-08-22 19:45:54 +0545

    all cards are done with styling

--- components/MoneyCard.tsx
+++ components/MoneyCard.tsx
@@ -1,28 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { ScrollView } from 'react-native-gesture-handler'
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { green } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
-
+import { View, Text } from "react-native";
+import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { CardsData } from "./utils/constants";
 
 const MoneyCard = () => {
   return (
     <ScrollView>
-        <View className=' m-5 flex-1 flex-col w-64  h-44 border-2 border-gray-200 bg-white rounded-lg shadow-xl'>
-      <View className='flex-1 flex-row justify-center my-2 '>
-        <FontAwesome color='gray' size={24} name='dollar'/>
-        <Text className='text-xl mx-2 text-gray-500 font-semibold'>Earnings</Text>
-      </View>
-      <View className='flex-1 flex-row justify-center items-center'>
-        <Text className='text-4xl'>$924.67</Text>
-        <Text>12%<FontAwesome name='arrow-up'color='green'/></Text>
-      </View>
-      <View>
-      <Text className='text-xl'>+112 than last Month</Text>
-      </View>
+      {CardsData.map((item) => (
+        <View
+          key={item?.amount}
+          className=" m-5 flex-1 flex-col w-64  h-44 border-2 border-gray-200 bg-white rounded-lg shadow-xl"
+        >
+          <View className="flex-1 flex-row justify-center my-2 ">
+            <FontAwesome color="gray" size={24} name="dollar" />
+            <Text className="text-xl mx-2 text-gray-500 font-semibold">
+              {item?.name}
+            </Text>
+          </View>
+          <View className="flex-1 flex-row justify-center items-center pl-3">
+            <Text className="text-5xl font-bold">${item?.amount}</Text>
+            <Text
+              className={`bg-gray-100 rounded text-md ${
+                item?.isincrease ? "text-green-600" : "text-red-600"
+              }  m-1`}
+            >
+              {item?.isincrease
+                ? item?.increasedPercent
+                : item?.decreasedPercent}
+              %
+              <FontAwesome
+                name={item?.isincrease ? "arrow-up" : "arrow-down"}
+                color={item?.isincrease ? "green" : "red"}              />
+            </Text>
+          </View>
+          <View className="flex-1 flex-row justify-center items-center">
+            <Text className={`text-lg text-center mx-1 bg-gray-100 ${
+                item?.isincrease ? "text-green-600" : "text-red-600"
+              } rounded-lg`}>
+              {item?.isincrease ? item?.increasedBy : item?.decreasedBy}
+            </Text>
+            <Text className="text-lg  text-gray-500 font-semibold">
+              than last Month
+            </Text>
+          </View>
         </View>
+      ))}
     </ScrollView>
-  )
-}
+  );
+};
 
-export default MoneyCard
+export default MoneyCard;
--- components/utils/constants.tsx
+++ components/utils/constants.tsx
@@ -1,1 +1,28 @@
 export const Logo=require("./pw.png");
+
+export const  CardsData=[
+  {
+    "name": "Earnings",
+    "isincrease": true,
+    "amount": 5000.00,
+    "increasedBy": "+$1000.00",
+    "increasedPercent": 25.00
+  },
+  {
+    "name": "Expenses",
+    "isincrease": false,
+    "amount": 2000.00,
+    "decreasedBy": "-$500.00",
+    "decreasedPercent": 20.00
+  },
+  {
+    "name": "Savings",
+    "isincrease": true,
+    "amount": 3000.00,
+    "increasedBy": "+$500.00",
+    "increasedPercent": 20.00
+  }
+]
+
+;
+  
